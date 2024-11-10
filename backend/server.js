2b033eadcf70c6b68af10dc1cc538ea41ab68b3b
Harshitha Lingamaneni <164991405+harshitha3008@users.noreply.github.com>
2024-11-10 11:58:53 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,13 @@ const recipeRoutes = require('./routes/recipeRoutes');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(cors(
+  {
+    origin :["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST","GET"],
+    credentials:true
+  }
+));
 
 // Debugging: Check if the URI is loaded
 console.log("MongoDB URI:", process.env.MONGODB_URI);
