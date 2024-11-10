d5195ea546c5d2f03fe722af604aed204c172651
Harshitha Lingamaneni <164991405+harshitha3008@users.noreply.github.com>
2024-11-10 12:40:07 +0530

    Update RegisterForm.js

--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -38,7 +38,7 @@ const RegisterForm = ({ closeModal }) => { // Added closeModal as a prop
     }
   
     try {
-      const response = await fetch('http://localhost:5000/api/auth/register', {
+      const response = await fetch('https://final-backend-2v7j.onrender.com/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
